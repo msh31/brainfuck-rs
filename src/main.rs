2d796c35252fe0a5f5063064506c81989cd4f7f1
marco <marco@marco007.dev>
2026-05-12 16:39:46 +0200

    copy some vector example from the docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    //what the fuck is this shit
+    let mut victor = vec![1, 2, 3];
+    victor.push(4);
+
+    let victoria = Vec::from([1, 2, 3, 4]);
+    assert_eq!(victor, victoria);
 }
