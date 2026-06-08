927fd1327b7ce5c144295c23ef68967ac62f9be2
marco <marco@marco007.dev>
2026-06-08 16:27:56 +0200

    complete this fucking project
    never, ever am i writing rust again.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,68 @@
+struct Interpreter {
+    tape: Vec<u8>, //memory - storage for the program since no vars
+    tc: usize,
+    program: String, //source
+    pc: usize,
+}
+
+impl Interpreter {
+    fn run(&mut self) {
+        while self.pc < self.program.len() {
+            //index into the program with the counter in ASCII
+            match self.program.as_bytes()[self.pc] {
+                b'>' => self.tc += 1,
+                b'<' => self.tc -= 1,
+
+                b'+' => self.tape[self.tc] += 1,
+                b'-' => self.tape[self.tc] -= 1,
+
+                b'.' => print!("{}", self.tape[self.tc] as char),
+                // b',' =>, //barely used so fuck that shit
+
+                b'[' => {
+                    if self.tape[self.tc] == 0 {
+                        let mut depth = 1;
+                        while depth > 0 {
+                            self.pc += 1;
+                            match self.program.as_bytes()[self.pc] {
+                                b'[' => depth += 1,
+                                b']' => depth -= 1,
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+                b']' => {
+                    if self.tape[self.tc] != 0 {
+                        let mut depth = 1;
+                        while depth != 0 {
+                            self.pc -= 1;
+                            match self.program.as_bytes()[self.pc] {
+                                b']' => depth += 1,
+                                b'[' => depth -= 1,
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+            self.pc += 1;
+        }
+    }
+}
+
+const MAX_TAPE_LENGTH: usize = 30000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut _fuck = Interpreter {
+        tape: vec![0; MAX_TAPE_LENGTH],
+        program: String::from(">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<+
++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-
+]<+."),
+        pc: 0,
+        tc: 0
+    };
 
-    //what the fuck is this shit
-    let mut victor = vec![1, 2, 3];
-    victor.push(4);
-
-    let victoria = Vec::from([1, 2, 3, 4]);
-    assert_eq!(victor, victoria);
+    _fuck.run();
 }
